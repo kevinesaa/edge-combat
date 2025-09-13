77dda6d46f0186c519ab4f380c299f4fceed8b2e
Kevin Esaa <kevin.esaa@gmail.com>
2025-09-13 19:22:11 -0400

    feat: only mobile screen inputs

---
+++ main-menu/scripts/MainMenuController.gd
@@ -0,0 +1,2 @@
+extends Node
+@onready var input_controller_node: MainMenuInputController = $inputController_node
---
+++ main-menu/scripts/MainMenuInputController.gd
@@ -0,0 +1,37 @@
+class_name MainMenuInputController
+extends Node
+
+var TACTIL_SCREEN_EVENTS:Array = (
+	[
+		InputEventScreenTouch, 
+		InputEventScreenDrag
+	]
+)
+
+func _ready() -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	
+	if(TACTIL_SCREEN_EVENTS.any(func(t): return is_instance_of(event,t))):
+		handledTactilScreen(event)
+		
+
+func handledTactilScreen(event: InputEvent):
+	
+	if(event.index==0):
+		
+		if(event is InputEventScreenTouch):
+			handledTouch(event)
+		
+		if(event is InputEventScreenDrag):
+			handledDraw(event)
+
+func handledTouch(event: InputEvent):
+	if(event.is_pressed()):
+		print("click/touch")
+	if(event.is_released()):
+		print("realeas: click/touch")
+
+func handledDraw(event: InputEvent):
+	print("drag")
