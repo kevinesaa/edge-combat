38e51987c6da3d0bb29c274ff6aa116cc387d858
Kevin Esaa <kevin.esaa@gmail.com>
2025-09-13 16:20:43 -0400

    intento de builder de state machine

--- _commons/scripts/CombatStateMachine.gd
+++ _commons/scripts/CombatStateMachine.gd
@@ -1,9 +1,10 @@
 class_name CombatStateMachine
 extends Node2D
 
-@export
+
 var s1:CombatMovement = ( 
 	CombatMovementBuilder.new()
-	
+	.setInitMovement(CombatMovement.CombatState.IDLE_CENTER)
+	.setMidMovement(CombatMovement.CombatState.PUNCH_RIGHT)
 	.build()
 )
