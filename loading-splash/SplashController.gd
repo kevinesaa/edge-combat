31aec40917900a82ff2b3c4d7bcb8980faa1665e
Kevin Esaa <kevin.esaa@gmail.com>
2025-09-13 16:57:37 -0400

    change scene - splash -> main

---
+++ loading-splash/SplashController.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@onready var change_scene_controller_node: ChangeSceneController = $changeSceneController_Node
+@onready var start_next_scene_timer: Timer = $start_next_scene_Timer
+
+func _ready() -> void:
+	start_next_scene_timer.start()
+	
+func start_next_scene_load():
+	change_scene_controller_node.load_next_scene()
+
+	
+func on_next_scene_is_ready_listener():
+	change_scene_controller_node.change_scene() 
