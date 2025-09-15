e1d9b3065ac9b5a01a696c29bb41d868e2bddf36
garces <agarces691@gmail.com>
2025-09-15 15:27:15 -0400

    add side movement

--- sandbox/scripts/PlayerInputKeybordSandbox.gd
+++ sandbox/scripts/PlayerInputKeybordSandbox.gd
@@ -9,10 +9,18 @@ func _ready():
 func _process(delta):
 	if(Input.is_action_just_pressed("right")):
 		notify_swipe_right()
-	pass
+	if(Input.is_action_just_pressed("left")):
+		notify_swipe_left()
+
 
 func notify_swipe_right():
 	var input:InputWrapper = InputWrapper.new()
 	input.initPosition= Vector2.ZERO
 	input.endPosition=Vector2.RIGHT
+	notify_swipe.emit(input)
+
+func notify_swipe_left():
+	var input:InputWrapper = InputWrapper.new()
+	input.initPosition= Vector2.ZERO
+	input.endPosition= Vector2.LEFT
 	notify_swipe.emit(input)
