2eb731bfb5c4e574f5a6df758f8627019472660a
Kevin Esaa <kevin.esaa@gmail.com>
2026-04-30 10:56:30 -0400

    feat: [WIP] character selection corners

---
+++ character-selector/CornerSelectionComponent.gd
@@ -0,0 +1,6 @@
+class_name CornerSelectionComponent
+extends Control
+
+var isSelected:bool = false
+@onready var backgroundPanel: Panel = $backgroundPanel
+@onready var textLabel: Label = $textLabel
